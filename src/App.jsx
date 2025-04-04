9ca79646b469c2e144e243359d98fd503cb9e458
Dev-KimDonghyun <hyungus723@gmail.com>
2025-04-04 10:47:05 +0900

    Feat: Start to Develop Frontend Login Logic

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import LoginMain from './pages/LoginMain.jsx';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <LoginMain />
+    </>
+  )
+}
+
+export default App;
---
+++ src/pages/LoginMain.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './../App.css';
+
+const LoginMain = () => {
+  return (
+    <div>LoginMain</div>
+  )
+}
+
+export default LoginMain;
